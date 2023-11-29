43c0ddaeed5c0c5e0d5736fac7bfba5ea70a48c2
HypeRRu <hyperru1lvl@gmail.com>
2023-11-29 20:43:29 +0300

    backend: disabled cors

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./router.js";
 import * as Db from "./db.js";
 
 const app = express();
 
 app.use( "/", router );
+app.use( cors() );
 app.listen( process.env.ANTIPLAGIAT_PORT );
 
 process.on( "SIGINT", async() => {
