7a936eff73eb166cf8ff76808552a1feb70d030e
Alekseev-Roman <alexeevroma2002@mail.ru>
2023-12-23 20:43:08 +0300

    add tooltips

--- frontend/src/components/pages/Navbar.vue
+++ frontend/src/components/pages/Navbar.vue
@@ -6,16 +6,16 @@
         <span class="title-2"> им. Марка Марковича </span>
       </div>
       <b-button-group>
-        <b-button @click="showStatistics" class="custom-button mr-2">
+        <b-button @click="showStatistics" class="custom-button mr-2" v-b-tooltip title="Сводная таблица">
           <img src="../../assets/tableIcon.svg"/>
         </b-button>
-        <b-button class="custom-button mr-2">
+        <b-button class="custom-button mr-2" v-b-tooltip title="Статистика">
           <img src="../../assets/chartIcon.svg"/>
         </b-button>
-        <b-button @click="download" class="custom-button mr-2">
+        <b-button @click="download" class="custom-button mr-2" v-b-tooltip title="Массовый импорт">
           <img src="../../assets/downloadIcon.svg"/>
         </b-button>
-        <b-button @click="exportData" class="custom-button">
+        <b-button @click="exportData" class="custom-button" v-b-tooltip title="Массовый экспорт">
           <img src="../../assets/exportIcon.svg"/>
         </b-button>
       </b-button-group>
